5fbd9eaec883bc8c85bc1e5b3ee825023c2263c2
Nate-Wessel <bike756@gmail.com>
2016-07-30 17:46:41 +0000

    initial upload of project

---
+++ etc/create_nb_tables.sql
@@ -0,0 +1,86 @@
+﻿/*
+create tables necessary for NextBus drivn realtime GTFS project
+*/
+
+/*
+DROP TABLE IF EXISTS nb_calendar_dates;
+CREATE TABLE nb_calendar_dates (
+    uid serial PRIMARY KEY,
+    service_id integer,
+    "date" date,
+    exception_type integer
+);
+*/
+
+DROP TABLE IF EXISTS nb_routes;
+CREATE TABLE nb_routes (
+    route_id varchar PRIMARY KEY,
+    route_short_name varchar, -- required
+    route_long_name varchar, -- required
+    route_type integer, -- required
+    title varchar -- not required
+);
+
+DROP TABLE IF EXISTS nb_stops;
+CREATE TABLE nb_stops (
+    uid serial PRIMARY KEY,
+    stop_id varchar,
+    stop_name varchar, -- required
+    stop_code integer, -- public_id
+    lon numeric,
+    lat numeric,
+    the_geom geometry(POINT,26917),
+    report_time timestamp with time zone
+);
+-- ignores insertion of records that duplicate either the tag or stopid fields
+--CREATE RULE "nb_stops_ignore_duplicate" AS ON INSERT TO nb_stops
+--    WHERE EXISTS( SELECT 1 FROM nb_stops WHERE stop_id = NEW.stop_id)
+--    DO INSTEAD NOTHING;
+
+DROP TABLE IF EXISTS nb_directions;
+CREATE TABLE nb_directions (
+    uid serial PRIMARY KEY,
+    route_id varchar,
+    direction_id varchar,
+    title varchar,
+    name varchar,
+    branch varchar,
+    useforui boolean,
+    stops text[],
+    report_time timestamp with time zone
+);
+CREATE INDEX ON nb_directions (direction_id);
+
+DROP TABLE IF EXISTS nb_vehicles;
+CREATE TABLE nb_vehicles (
+	trip_id integer,
+	seq integer, -- sequence in the trip reports
+	report_time double precision, -- epoch time of report
+	lat numeric,
+	lon numeric,
+	location geometry(Point,26917)
+);
+CREATE INDEX ON nb_vehicles (trip_id);
+
+DROP TABLE IF EXISTS nb_trips;
+CREATE TABLE nb_trips (
+    trip_id integer PRIMARY KEY,
+    route_id integer,
+    direction_id varchar(35),
+    service_id integer,
+    vehicle_id integer,
+    match_confidence real,
+    match_geom geometry(LINESTRING,26917) -- map-matched route geometry
+);
+
+DROP TABLE IF EXISTS nb_stop_times;
+CREATE TABLE nb_stop_times(
+    uid serial PRIMARY KEY,
+    trip_id integer,
+    stop_id varchar,
+    stop_sequence integer,
+    etime double precision, -- epoch time at greenwich
+    arrival_time interval HOUR TO SECOND,
+    departure_time interval HOUR TO SECOND
+);
+
