fd28f94fd6f6c443957dcc83fec97c7290cad9c5
Nate-Wessel <bike756@gmail.com>
2016-07-31 10:10:39 -0400

    added comments to configuration file

--- conf.py
+++ conf.py
@@ -1,14 +1,20 @@
+# CONFIGURATION FILE
+# set the parameters unique to your setup below
+
 conf = {
+	# database connnection
 	'db':
 		{
 			'host':'localhost',
-			'dbname':'',
-			'user':'',
-			'password': ''
+			'name':'',
+			'user':'postgres',
+			'password':''
 		},
+	# agency tag for nextbus API
 	'agency':'ttc',
+	# Where is the ORSM server? 
 	'OSRMserver':{
-		'url':'',
+		'url':'http://???/match/v1/transit/',
 		'timeout':10
 	}
 }
