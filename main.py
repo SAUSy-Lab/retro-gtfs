a86f6fce80b2e81d7897335af86270d5b937d78d
Nate-Wessel <bike756@gmail.com>
2017-03-13 04:03:58 +0000

    created command line args for turning certain features on and off

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from nb_api import get_new_vehicles, fetch_route, all_routes
 import db
 from time import sleep
 import random
+import sys
+
+# takes arguments from the command line
+# should we get route information from the API? default False
+getRoutes = True if 'getRoutes' in sys.argv else False
+# should we process trips (or simply store the vehicles)? default False
+processTrips = True if 'processTrips' in sys.argv else False
+# should existing data be truncated? default False;
+truncateData = True if 'truncateData' in sys.argv else False
+
 
 def time_loop():
 	"""timer function whose purpose is to call itself every N seconds 
@@ -13,27 +23,29 @@ def time_loop():
 	threading.Timer( 10, time_loop ).start() # int is delay in seconds
 	# request neew vehicles and store them
 	get_new_vehicles()
-	# request a route update at random, with p = 0.1
-	if random.random() <= 0.1:
-		fetch_route( random.choice(routes) )
+	if getRoutes:
+		# request a route update at random, with p = 0.1
+		if random.random() <= 0.1:
+			fetch_route( random.choice(routes) )
 
-# clear previous data
-db.empty_tables()
+if truncateData:
+	db.empty_tables()
 
-# get all the route data, afresh
-# threading this makes it faster
-#print 'requesting all route data'
-routes = all_routes()
-for route_id in routes:
-	t = threading.Thread(target=fetch_route,args=(route_id,))
-	t.start()
-	if threading.active_count() >= 20:
-		sleep(3)
+if getRoutes:
+	# get all the route data, afresh
+	# threading this makes it faster
+	#print 'requesting all route data'
+	routes = all_routes()
+	for route_id in routes:
+		t = threading.Thread(target=fetch_route,args=(route_id,))
+		t.start()
+		if threading.active_count() >= 20:
+			sleep(3)
 
-sleep(10)
+	sleep(10)
 
 # call the big function. This takes longer to run the first time, 
-get_new_vehicles()
+get_new_vehicles(processTrips)
 
 # so wait a bit longer than usual to call the timer function 10secs later
 threading.Timer( 10, time_loop ).start()
