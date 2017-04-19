07bd82320737d7bb57d4bac9cad3be16c1fc7115
Nate-Wessel <bike756@gmail.com>
2017-04-19 07:20:49 -0400

    indexes speed things up enourmously. Also clustering now just for good measure

--- etc/create_nb_tables.sql
+++ etc/create_nb_tables.sql
@@ -24,7 +24,8 @@ CREATE TABLE nb_stops (
 	the_geom geometry(POINT,26917),
 	report_time timestamp with time zone
 );
-CREATE INDEX ON nb_stops (stop_id);
+CREATE nbs_idx INDEX ON nb_stops (stop_id);
+CLUSTER nb_stops USING nbs_idx;
 
 DROP TABLE IF EXISTS nb_directions;
 CREATE TABLE nb_directions (
@@ -51,7 +52,8 @@ CREATE TABLE nb_vehicles (
 	location geometry(Point,26917),
 	ignore boolean DEFAULT FALSE -- ignore this vehicle during processing?
 );
-CREATE INDEX ON nb_vehicles (trip_id);
+CREATE INDEX nbv_idx ON nb_vehicles (trip_id);
+CLUSTER nb_vehicles USING nbv_idx;
 
 DROP TABLE IF EXISTS nb_trips;
 CREATE TABLE nb_trips (
@@ -63,12 +65,13 @@ CREATE TABLE nb_trips (
 	block_id integer,
 	match_confidence real,
 	match_geom geometry(LINESTRING,26917), -- map-matched route geometry
-	orig_geom  geometry(LINESTRING,26917),	-- geometry of all points
-	clean_geom geometry(LINESTRING,26917), -- geometry of points used in map matching
+	orig_geom  geometry(LINESTRING,26917),	-- geometry of all points TODO kill
+	clean_geom geometry(LINESTRING,26917), -- geometry of points used in map matching TODO kill
 	problem varchar DEFAULT '',	-- description of any problems that arise
 	ignore boolean DEFAULT FALSE	-- ignore this vehicle during processing?
 );
-CREATE INDEX ON nb_trips (trip_id);
+CREATE nbt_idx INDEX ON nb_trips (trip_id);
+CLUSTER nb_trips USING nbt_idx;
 
 DROP TABLE IF EXISTS nb_stop_times;
 CREATE TABLE nb_stop_times(
@@ -80,4 +83,6 @@ CREATE TABLE nb_stop_times(
 	arrival_time interval HOUR TO SECOND,
 	departure_time interval HOUR TO SECOND
 );
+CREATE INDEX nbst_idx ON nb_stop_times (trip_id);
+CLUSTER nb_stop_times USING nbst_idx;
 
