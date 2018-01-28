f585423bc7827bbd296859b4b491585b1c619ab7
Nate-Wessel <bike756@gmail.com>
2018-01-27 21:38:36 -0500

    expanded documentation of sample configuration file

--- sample_conf.py
+++ sample_conf.py
@@ -6,27 +6,33 @@
 from functools import partial
 import pyproj
 
+# this must be a meter-based projection appropriate for your region
+# UTM projections are suggested. 
+PROJECT_EPSG = 32723
+
 conf = {
 	# PostgreSQL database connnection
 	'db':
 		{
 			'host':'localhost',
-			'name':'',
+			'name':'', # database name
 			'user':'',
 			'password':'',
 			'tables':{
 				# these are SQL-safe table names used directly in queries
+				# if you set up the tables with create_nb_tables.sql, you have likely changed the prefix
 				'trips':'prefix_trips',
 				'stops':'prefix_stops',
 				'stop_times':'prefix_stop_times',
 				'directions':'prefix_directions'
 			}
 		},
-	# agency tag for the Nextbus API
+	# agency tag for the Nextbus API, which can be found at
+	# http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList
 	'agency':'ttc',
 	# Where is the ORSM server? Give the root url
 	'OSRMserver':{
-		'url':'http://201.167.182.17:5002',
+		'url':'http://localhost:5002',
 		'timeout':10 # seconds
 	},
 	# function for projecting from lat-lon for shapely
@@ -35,11 +41,13 @@ conf = {
 	'projection':partial(
 		 pyproj.transform,
 		 pyproj.Proj('+init=EPSG:4326'),
-		 pyproj.Proj('+init=EPSG:32723')
+		 pyproj.Proj('+init=EPSG:'+str(PROJECT_EPSG))
 	),
-	'localEPSG':32723,
+	'localEPSG':PROJECT_EPSG,
+	# timezone is given as the hours offset from Greenwich mean time
 	'timezone':-4,
-	# distance threshold for stop matching in meters
+	# distance threshold for stop matching in meters; stops more than this far 
+	# away from the matched route will not be included
 	'stop_dist':30,
 	# estimated GPS error radius in meters
 	# this applies to all points and effects map-matching
