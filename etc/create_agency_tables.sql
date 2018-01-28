bd2ed7df85f0e59c2544d6e188bb9d9b363ff174
Nate-Wessel <bike756@gmail.com>
2018-01-27 22:13:53 -0500

    made table creation script more flexible

--- etc/create_agency_tables.sql
+++ etc/create_agency_tables.sql
@@ -1,15 +1,24 @@
 ﻿/*
-	This defines the schema necessary for NextBus driven realtime GTFS project, 
-	in the devlopment case, for the Toronto Transit Commission. 
-	Table names may be changed, but this must be indicated in conf.py.
+	This script defines/creates the necessary database schema. It uses psql 
+	variables and should be run with psql on a PostgreSQL database with 
+	PostGIS installed. You'll need to set the projection for your region 
+	and optionally provide a table name prefix or otherwise change the table 
+	names to distinguish among agencies within a database.
 */
 
+-- this should be a meter based projection like UTM
+\set EPSG 26917
+-- set your table names here, and again in config.py
+\set stops_table       prefix_stops
+\set directions_table  prefix_directions
+\set trips_table       prefix_trips
+\set stop_times_table  prefix_stop_times
 
 /*
 	equivalent to GTFS stops table
 */
-DROP TABLE IF EXISTS ttc_stops;
-CREATE TABLE ttc_stops (
+DROP TABLE IF EXISTS :stops_table;
+CREATE TABLE :stops_table (
 	uid serial PRIMARY KEY,
 	stop_id varchar,
 	stop_name varchar, -- required
@@ -20,15 +29,16 @@ CREATE TABLE ttc_stops (
 	report_time double precision, -- epoch time
 	active boolean DEFAULT TRUE -- debugging flag
 );
-CREATE INDEX ON ttc_stops (stop_id);
+CREATE INDEX ON :stops_table (stop_id);
+
 
 /*
 	Similar to GTFS trips table, in that it stores sequences of 
 	stops to be served by a trip and these can be matched to a 
 	direction_id on a particular vehicle
 */
-DROP TABLE IF EXISTS ttc_directions;
-CREATE TABLE ttc_directions (
+DROP TABLE IF EXISTS :directions_table;
+CREATE TABLE :directions_table (
 	uid serial PRIMARY KEY,
 	route_id varchar,
 	direction_id varchar,
@@ -39,15 +49,15 @@ CREATE TABLE ttc_directions (
 	stops text[],
 	report_time double precision -- epoch time
 );
-CREATE INDEX ON ttc_directions (direction_id);
+CREATE INDEX ON :directions_table (direction_id);
 
 /*
 	Data on vehilce locations fetched from the API gets stored here along 
 	with map-matched geometries. When extracted into GTFS, most feilds here 
 	are ignored. "Trips" are the primary object of the data processing sequence.  
 */
-DROP TABLE IF EXISTS ttc_trips;
-CREATE TABLE ttc_trips (
+DROP TABLE IF EXISTS :trips_table;
+CREATE TABLE :trips_table (
 	trip_id integer PRIMARY KEY,
 	orig_geom geometry(LINESTRING,26917),	-- geometry of all points
 	times double precision[], -- sequential report_times, corresponding to points on orig_geom
@@ -64,16 +74,16 @@ CREATE TABLE ttc_trips (
 	problem varchar DEFAULT '', -- description of any problems that arise
 	active boolean DEFAULT TRUE -- debugging flag
 );
-CREATE INDEX ON ttc_trips (trip_id);
+CREATE INDEX ON :trips_table (trip_id);
 
 /*
 	Where interpolated stop times are stored for each trip. 
 */
-DROP TABLE IF EXISTS ttc_stop_times;
-CREATE TABLE ttc_stop_times(
+DROP TABLE IF EXISTS :stop_times_table;
+CREATE TABLE :stop_times_table (
 	trip_id integer,
 	stop_id varchar,
 	stop_sequence integer,
 	etime double precision -- non-localized epoch time in seconds
 );
-CREATE INDEX ON ttc_stop_times (trip_id);
+CREATE INDEX ON :stop_times_table (trip_id);
