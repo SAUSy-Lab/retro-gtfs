bd33d2786105d8d1997e798c6ec5abab51fc8aba
Nate-Wessel <bike756@gmail.com>
2018-04-09 11:21:33 -0400

    combined views

--- debug/trip-views.sql
+++ debug/trip-views.sql
@@ -1,11 +1,14 @@
-/*For viewing stop_times table data e.g. in QGIS*/
+/* DB views for viewing and debugging trip-level data e.g. in QGIS */
 
 -- set your table names here
 \set stops_table       jv_stops
 \set stop_times_table  jv_stop_times
+\set directions_table  jv_directions
+
+
+-- Adds geometry to stop_times table
 
 DROP VIEW IF EXISTS stop_times_view;
-
 CREATE OR REPLACE VIEW stop_times_view AS 
 SELECT 
 	DISTINCT ON (st.trip_id, st.stop_id) st.stop_id,
@@ -17,5 +20,18 @@ FROM :stop_times_table AS st
 JOIN :stops_table AS s 
 	ON s.stop_id = st.stop_id AND
 	s.report_time <= st.etime
-ORDER BY st.stop_id, st.trip_id, s.report_time
+ORDER BY st.stop_id, st.trip_id, s.report_time;
 
+
+-- Gives sets of stops with geometry from the directions table
+
+DROP VIEW IF EXISTS direction_stops_view;
+﻿CREATE OR REPLACE VIEW direction_stops_view AS 
+SELECT 
+	d.direction_id,
+	a.stop, 
+	a.seq,
+	s.the_geom,
+	row_number() OVER () AS uid
+FROM :directions_table AS d, unnest(d.stops) WITH ORDINALITY a(stop, seq)
+JOIN :stops_table AS s ON s.stop_id = a.stop;
