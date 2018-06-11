da8ec34913e92524336acba3fecd66cf38fbc308
Nate-Wessel <bike756@gmail.com>
2018-06-11 13:32:26 -0400

    sort by match

--- debug/route-quality-measures.sql
+++ debug/route-quality-measures.sql
@@ -45,7 +45,7 @@ SELECT
 	-- average confidence of matched trips (including default = 1)
 	round( AVG(match_confidence)::numeric, 4 ) AS avg_confidence,
 	-- trip_id's of trips making less than half of stops
-	( array_agg(t.trip_id ORDER BY random()) FILTER (WHERE sm.num_stops < sg.num_stops/2) )[1:3] AS too_few_stops,
+	( array_agg(t.trip_id ORDER BY match_confidence * random() ASC ) FILTER (WHERE sm.num_stops < sg.num_stops/2) )[1:3] AS too_few_stops,
 	-- trip_id's of trips making more stops than scheduled
 	( array_agg(t.trip_id ORDER BY random()) FILTER (WHERE sm.num_stops > sg.num_stops) )[1:3] AS too_many_stops
 FROM :trips_table AS t 
@@ -53,5 +53,4 @@ JOIN stops_made AS sm ON t.trip_id = sm.trip_id
 JOIN stops_given AS sg ON t.trip_id = sg.trip_id
 WHERE t.problem NOT IN ('too short','too few vehicles') OR t.problem IS NULL
 GROUP by route_id 
-ORDER BY num_trips DESC
-
+ORDER BY avg_confidence ASC;
