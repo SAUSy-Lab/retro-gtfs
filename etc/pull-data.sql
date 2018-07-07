147756cc964eaec30e0c64717c6d518a276567be
Nate-Wessel <bike756@gmail.com>
2018-07-07 19:59:58 -0400

    added service_id option to data pull script

--- etc/pull-data.sql
+++ etc/pull-data.sql
@@ -10,25 +10,27 @@
 */
 
 -- set your table name prefix
-\set prefix					'ttc_'
+\set prefix            'ttc_'
+-- set your service_id as a row_record, i.e. '(1,2,3)'
+\set service_ids       '(17476)'
 
 -- set the table names
-\set stops_table			:prefix'stops'
-\set directions_table	:prefix'directions'
-\set trips_table			:prefix'trips'
-\set stop_times_table	:prefix'stop_times'
+\set stops_table       :prefix'stops'
+\set directions_table  :prefix'directions'
+\set trips_table       :prefix'trips'
+\set stop_times_table  :prefix'stop_times'
 -- timezone
-\set tz						'America/Toronto'
+\set tz                'America/Toronto'
 -- where to save the output
-\set outdir					'/home/nate/retro-gtfs/output/ttc/'
+\set outdir            '/home/nate/retro-gtfs/output/ttc/'
 -- stop configuration past here... just setting output locations from 
 -- the above because concatenation is complicated
-\set calendar_dates		:outdir'calendar_dates.txt'
-\set stops					:outdir'stops.txt'
-\set routes					:outdir'routes.txt'
-\set trips					:outdir'trips.txt'
-\set stop_times			:outdir'stop_times.txt'
-\set shapes					:outdir'shapes.txt'
+\set calendar_dates    :outdir'calendar_dates.txt'
+\set stops             :outdir'stops.txt'
+\set routes            :outdir'routes.txt'
+\set trips             :outdir'trips.txt'
+\set stop_times        :outdir'stop_times.txt'
+\set shapes            :outdir'shapes.txt'
 
 
 -- set the service_id of trips based on the time of their first stop
@@ -73,7 +75,6 @@ WHERE
 	-- only changed or null values
 	( st.fake_stop_id != sub.fake_id OR st.fake_stop_id IS NULL );
 
-
 -- make calendar_dates.txt
 \echo 'Exporting calendar.txt'
 COPY(
@@ -82,7 +83,8 @@ COPY(
 		to_char(TIMESTAMP 'EPOCH' + (service_id * INTERVAL '1 day'),'YYYYMMDD') AS date,
 		1 AS exception_type
 	FROM :trips_table 
-	WHERE NOT ignore
+	WHERE NOT ignore AND 
+	service_id IN :service_ids
 	ORDER BY service_id ASC
 ) TO :'calendar_dates' CSV HEADER;
 
@@ -97,8 +99,12 @@ COPY (
 		s.stop_name,
 		s.lat AS stop_lat,
 		s.lon AS stop_lon
-	FROM :stop_times_table AS st JOIN :stops_table AS s 
-		ON s.uid = st.stop_uid
+	FROM :trips_table AS t 
+	JOIN :stop_times_table AS st ON
+		t.trip_id = st.trip_id
+	JOIN :stops_table AS s ON 
+		s.uid = st.stop_uid
+	WHERE t.service_id IN :service_ids
 ) TO :'stops' CSV HEADER;
 
 
@@ -112,7 +118,7 @@ COPY (
 			'' AS route_long_name,
 			3 AS route_type -- LET THEM RIDE BUSES
 	FROM :trips_table
-	WHERE NOT ignore
+	WHERE service_id IN :service_ids AND NOT ignore
 ) TO :'routes' CSV HEADER;
 
 
@@ -125,7 +131,7 @@ COPY (
 		t.block_id,
 		'shp_'||trip_id AS shape_id
 	FROM :trips_table AS t
-	WHERE NOT ignore
+	WHERE service_id IN :service_ids AND NOT ignore
 ) TO :'trips' CSV HEADER;
 
 
@@ -149,7 +155,7 @@ COPY (
 		stop_sequence,
 		fake_stop_id AS stop_id
 	FROM :stop_times_table AS st JOIN :trips_table AS t ON st.trip_id = t.trip_id
-	WHERE NOT t.ignore -- not actually necessary
+	WHERE service_id IN :service_ids AND NOT t.ignore 
 	ORDER BY trip_id, stop_sequence ASC
 ) TO :'stop_times' CSV HEADER;
 
@@ -167,6 +173,6 @@ COPY (
 		'shp_'||trip_id AS shape_id,
 		(ST_DumpPoints(ST_Simplify(match_geom,10))).*
 	FROM :trips_table
-	WHERE NOT ignore) AS sub
+	WHERE service_id IN :service_ids AND NOT ignore ) AS sub
 ) TO :'shapes' CSV HEADER;
 
